7496e98000605e1765357d6c1d31e5029a03123b
Chace Powell <cpcpowell8@gmail.com>
2018-07-30 21:23:37 -0400

    add vars for ajax call. console logs out correctly.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,4 @@
 
-
-var search = 'test';
-var recordNum = 2;
-var startYear = 2010;
-var endYear = 2018;
-
 // <---------------adding click events------------------------->
 var searchParameters = [];
 searchParameters = $("#exampleInputEmail1").html;
@@ -15,24 +9,25 @@ endYear = $("#endyearinput").html;
 var recordNumber = [];
 recordNumber = $("#exampleFormControlSelect1").html;
 $("#searchButton").on("click", function() {
+    var search = $('#exampleInputEmail1').val();
+    var recordNum = $('#exampleFormControlSelect1').val();
+    var startYear = $('#yearinput').val();
+    var endYear = $('#endyearinput').val();
+    var queryURL =
+        `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=b43250bb6ea945bbbcbee004241d4b01&q=${search}&begin_date=${startYear}&end_date=${endYear}`;
 
+    $.ajax({
+        url: queryURL
+    }).then(function (res) {
+        var arr = res.response.docs;
+        for (var i = 0; i < recordNum; i++) {
+            console.log(arr[i]);
+        }
+    });
 });
 $("#clearButton").on("click", function() {
 searchParameters = [];
 startYear = [];
 endYear = [];
 recordNumber = [1];
-});
-var search = $(this).attr('data-name');
-
-var queryURL =
-  'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=b43250bb6ea945bbbcbee004241d4b01&q=test&begin_date=20001012&end_date=20081209';
-
-$.ajax({
-    url: queryURL
-}).then(function(res) {
-    var arr = res.response.docs;
-    for (var i = 0; i < recordNum; i++) {
-        console.log(arr[i]);
-    }
 });
