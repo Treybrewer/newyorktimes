403722bbee0c80f4db199c921971ed3c197786cc
Chace Powell <cpcpowell8@gmail.com>
2018-07-30 20:58:27 -0400

    add parameters to ajax call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,16 @@
 
-var search = $(this).attr('data-name');
+var search = 'test';
+var recordNum = 2;
+var startYear = 2010;
+var endYear = 2018;
 var queryURL =
-  'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=b43250bb6ea945bbbcbee004241d4b01&q=' + search;
+  'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=b43250bb6ea945bbbcbee004241d4b01&q=test&begin_date=20001012&end_date=20081209';
 
 $.ajax({
     url: queryURL
-})
-.then(function(res) {
-    var arr = res.docs;
-    for (var i = 0; i < arr.length; i++) {
-        console.log(arr[i].headline.main);
+}).then(function(res) {
+    var arr = res.response.docs;
+    for (var i = 0; i < recordNum; i++) {
+        console.log(arr[i]);
     }
 });
