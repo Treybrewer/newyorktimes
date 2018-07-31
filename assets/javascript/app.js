d42ca23a14b9d9ad2f91e88a868fc901e33bdca8
Odin Almighty <36708782+Treybrewer@users.noreply.github.com>
2018-07-30 20:55:00 -0400

    adding id's and click events

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,19 @@
 
+// <---------------adding click events------------------------->
+var searchParameters = [];
+searchParameters = $("#exampleInputEmail1").html;
+var startYear = [];
+startYear = $("#yearinput").html;
+var endYear = [];
+endYear = $("#endyearinput").html;
+$("#searchButton").on("click", function() {
+
+});
+$("#clearButton").on("click", function() {
+searchParameters = [];
+startYear = [];
+endYear = [];
+});
 var search = $(this).attr('data-name');
 var queryURL =
   'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=b43250bb6ea945bbbcbee004241d4b01&q=' + search;
